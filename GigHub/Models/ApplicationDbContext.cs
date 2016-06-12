4327ae03a9088a4b187fab42bfbf1084b57414c9
ILya-Lev <lis2889@gmail.com>
2016-06-12 16:07:43 +0300

    Added domain classes - Gig and Genre

---
+++ GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GigHub.Models
+{
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		//to notify EF about new custom model class - Gig;
+		//it also contains references to class Genre - so EF will handle it automatically
+		public DbSet<Gig> Gigs { get; set; }
+		public DbSet<Genre> Genres { get; set; }
+
+		public ApplicationDbContext()
+			: base("DefaultConnection", throwIfV1Schema: false)
+		{
+		}
+
+		public static ApplicationDbContext Create()
+		{
+			return new ApplicationDbContext();
+		}
+	}
+}
