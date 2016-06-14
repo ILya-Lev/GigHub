2d73a63e5b4fb24d61e854f227da5a04424242bc
illia.levandovskyi <lis2889@gmail.com>
2016-06-14 13:18:38 +0300

    Add a Gig form validation (server and client side) implementation; known defect - no client side validation of custom attributes added

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,16 +1,31 @@
 ﻿using GigHub.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.ViewModels
 {
 	public class GigFormViewModel
 	{
+		[Required]
 		public string Venue { get; set; }
+
+		[Required]
+		[FutureDate]
 		public string Date { get; set; }
+
+		[Required]
+		[ValidTime]
 		public string Time { get; set; }
+
+		[Required]
 		public byte Genre { get; set; }
+
 		public IEnumerable<Genre> Genres { get; set; }
-		public DateTime DateTime => DateTime.Parse($"{Date} {Time}");
+
+		// if it is a prop MVC tries to construct the VM instance in parameter of controller
+		// but it could not be valid, so make it a method in order to avoid exception generation
+		// while we expect the case when DateTime could not be correctly created
+		public DateTime DateTime () => System.DateTime.Parse($"{Date} {Time}");
 	}
 }
